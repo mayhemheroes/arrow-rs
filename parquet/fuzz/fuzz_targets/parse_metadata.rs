5ba4afe0d26e0ca5948aba43c125ae1ed0b4b60b
Aziz Rizayev <rizaziz2002@forallsecure.com>
2026-04-09 18:19:17 +0000

    Fix

--- parquet/fuzz/fuzz_targets/parse_metadata.rs
+++ parquet/fuzz/fuzz_targets/parse_metadata.rs
@@ -1,6 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    let _ = parquet::file::footer::decode_metadata(data);
+use parquet::file::metadata::ParquetMetaDataReader;
+fuzz_target!(|data: &[u8]| {    
+    let _ = ParquetMetaDataReader::decode_metadata(data);
 });
